1f9d8f165e08fa5c227c32c61ca563489592fac4
Edward Paget <ed.paget@gmail.com>
2013-07-22 16:33:33 -0500

    Setup Repo

---
+++ libubret.js
@@ -0,0 +1,18 @@
+// libubret
+// libubret 0.2.0
+// libubret is distributed under the APL. See COPYING
+
+(function () {
+
+  var root = this;
+  var previousUbret = root.Ubret;
+
+  var Ubret = {};
+
+  if (typeof exports !== 'undefined')
+    module.exports = Ubret;
+  else
+    root.Ubret = Ubret;
+
+  Ubret.Events = {}
+}).call(this)
